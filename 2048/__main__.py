862240153b9336a65320e274ff12c181f3821e91
Samuel Ochoa <samuelochoap@gmail.com>
2020-05-23 13:09:04 -0400

    Add win argument, rename size argument

--- 2048/__main__.py
+++ 2048/__main__.py
@@ -3,12 +3,14 @@ import argparse
 
 def main(args):
     # TODO: Actual game
-    print(f'2048 game: {args["n"]}x{args["n"]} board.')
+    print(f'{args["win"]} game: {args["size"]}x{args["size"]} board.')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='2048 game.')
-    parser.add_argument('n', metavar='N', type=int, nargs='?',
-                        default=4, help='nxn will be the size of the board')
+    parser.add_argument('size', metavar='N', type=int, nargs='?',
+                        default=4, help='NxN will be the size of the board')
+    parser.add_argument('win', metavar='W', type=int, nargs='?',
+                        default=2048, help='cell number needed to win')
     args = parser.parse_args()
     main(vars(args))
