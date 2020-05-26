aec7f61d17e25a8396c23f9664998f5238c2cb35
Samuel Ochoa <samuelochoap@gmail.com>
2020-05-26 18:00:00 -0400

    Game defaults to GUI version, add --cli option

--- 2048/__main__.py
+++ 2048/__main__.py
@@ -1,10 +1,12 @@
 import argparse
-from . import cli
+from . import cli, gui
 
 
 def main(args):
-    # Run the main function of the CLI game
-    cli.main(args['size'], args['win'])
+    if args['cli']:
+        cli.main(args['size'], args['win'])
+    else:
+        gui.main(args['size'], args['win'])
 
 
 if __name__ == '__main__':
@@ -13,5 +15,7 @@ if __name__ == '__main__':
                         default=4, help='NxN will be the size of the board')
     parser.add_argument('win', metavar='W', type=int, nargs='?',
                         default=2048, help='cell number needed to win')
+    parser.add_argument('--cli', action='store_true',
+                        help='run command-line version of the game')
     args = parser.parse_args()
     main(vars(args))
